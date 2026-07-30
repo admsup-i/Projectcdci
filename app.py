339c7bfa8c127a7f3f033b74df07e9ed3fa9876e
admsup-i <admsup78234@gmail.com>
2026-07-30 15:09:04 +0530

    final

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ APP_VERSION = "1.0.0"
 @app.route("/")
 def home():
     return jsonify({
-       "message": "Hello from the CI/CD demo app! iliyas Siddiqui",
+       "message": "Hello from the CI/CD demo app! iliyas Ahemad Siddiqui",
         "version": APP_VERSION
     })
 
